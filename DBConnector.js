075eba9ff94b779c5ed199515fb484f01061d21f
ks-wook <ksu864@gmail.com>
2023-04-30 01:18:11 +0900

    DB 연동 코드 업데이트

--- DBConnector.js
+++ DBConnector.js
@@ -1,21 +1,106 @@
 const mysql = require('mysql2/promise');
+const config = require('./config.json');
+
 
 function connectDatabase() {
   const connection = mysql.createConnection({
     host: '127.0.0.1',
-    user: 'root',
-    password: 'your password',
+    user: config.id,
+    password: config.password,
     database: 'mainstationdb'
   });
 
   return connection;
 }
 
-async function insertUser() {
 
 
-    const query = `INSERT INTO user (name, ID) VALUES (?, ${generateID('user')})`;
-    const values = ['테스트유저'];
+async function select_db(table, condition) {
+
+    const connection = await connectDatabase();
+
+    if(condition != null) {
+        // condition = Buffer.from(condition).toString('hex');
+        condition = condition.toString('hex');
+        console.log(condition);
+    }
+    try { // TODO : 다른 모든 테이블에 대해서 select문 처리 작성
+        if(table == 'user') {
+
+            const [results, fields] = await connection.execute(`SELECT ID, name FROM ${table} WHERE id = x'${condition}'`);
+            return results;
+            // return find_data(results, condition);
+        }
+        else if(table == 'device') {
+            const [results, fields] = await connection.execute(`SELECT ID, State, OwnerId FROM ${table} WHERE id = x'${condition}'`);
+            return results;
+            // return find_data(results, condition);
+        }
+        else if(table == 'beacon') {
+            const [results, fields] = await connection.execute(`SELECT ID, State, SpaceId FROM ${table} WHERE id = x'${condition}'`);
+            return results;
+            // return find_data(results, condition);
+        }
+        else if(table == 'router') {
+            const [results, fields] = await connection.execute(`SELECT ID, name, MacAdr FROM ${table} WHERE id = x'${condition}'`);
+            return results;
+            // return find_data(results, condition);
+        }
+        else if(table == 'space') {
+            const [results, fields] = await connection.execute(`SELECT ID, name FROM ${table} WHERE id = x'${condition}'`);
+            return results;
+            // return find_data(results, condition);
+        }
+        else {
+            console.log('there are no table');
+            return results;
+            // return null;
+        }
+    } catch (error) {
+        console.error('Error inserting data:', error);
+    } finally {
+        connection.end();
+    }
+}
+
+
+// data에 넣고자 하는 데이터의 정보가 들어있다.
+async function insert_db(data) {
+
+    var query = null;
+    var values = null;
+
+    var genearted_id = generateID(`${data.table}`);
+
+    if(data.table == 'user') {
+        query = `INSERT INTO user (ID, name) VALUES (${genearted_id}, ?)`;
+        values = [data.name];
+    }
+    else if(data.table == 'device') {
+        const State = Buffer.from([0x00, 0x00]);
+        const OwnerId = Buffer.from(data.OwnerId.toString('hex'), 'hex');
+        query = `INSERT INTO device (ID, State, OwnerId) VALUES (${genearted_id}, ?, ?)`;
+        values = [State, OwnerId];
+    }
+    else if(data.table == 'beacon') {
+        const State = Buffer.from([0x00, 0x00]);
+        const SpaceId = Buffer.from(data.SpaceId.toString('hex'), 'hex');
+        query = `INSERT INTO beacon (ID, State, SpaceId) VALUES (${genearted_id}, ?, ?)`;
+        values = [State, SpaceId];
+    }
+    else if(data.table == 'router') {
+        const MacAdr = Buffer.from(data.MacAdr.toString('hex'), 'hex');
+        query = `INSERT INTO router (ID, name, MacAdr) VALUES (${genearted_id}, ?, ?)`;
+        values = [data.name, MacAdr];
+    }
+    else if(data.table == 'space') {
+        query = `INSERT INTO space (ID, name) VALUES (${genearted_id}, ?)`;
+        values = [data.name];
+    }
+    else {
+        console.log('error : there are no table');
+        return;
+    }
 
     const connection = await connectDatabase();
     
@@ -26,9 +111,70 @@ async function insertUser() {
         console.error('Error inserting data:', error);
     } finally {
         connection.end();
+        return genearted_id;
     }
 
 }
+
+async function update_db(data) {
+
+    var update_item_id = null;
+
+    if(data.update_item_id != null) {
+        update_item_id = data.update_item_id.toString('hex');
+        console.log(update_item_id);
+    }
+    else {
+        console.log('error : there are no update id');
+        return;
+    }
+
+    const sql = `UPDATE ${data.table} SET ${data.column} = ? WHERE id = x'${update_item_id}'`;
+
+    // 바이트 배열의 수정인 경우 DB처리를 위해 문자열 변환 -> 바이트 배열 전환 과정을 서버에서 거쳐야함
+    // 클라이언트에서 받은 바이트 배열은 바로 처리가 불가능
+    if(data.column == 'State' || data.column == 'OwnerId' || data.column == 'MacAdr') {
+        data.new_value = Buffer.from(data.new_value.toString('hex'), 'hex');
+    }
+
+    const values = [data.new_value];
+
+    const connection = await connectDatabase();
+
+    try {
+        
+        const [rows, fields] = await connection.execute(sql, values);
+
+        console.log(`${data.column} row(s) update complete`);
+    } catch (error) {
+        console.error(`Error updating data: ${error}`);
+    }
+
+    return update_item_id;
+}
+
+
+
+// function find_data(results, string_id) {
+
+//     if(string_id == null)
+//       return results;
+
+//     var find_result = null;
+//     results.forEach(result => {
+//       const binaryString = Buffer.from(result.ID).toString('hex');
+//       // console.log(binaryString);
+//       // console.log(string_id);
+//       if(binaryString == string_id) {
+//           console.log('Data searched !!');
+//           find_result = result;
+//       }
+      
+//     });
+
+//     return find_result;
+
+// }
   
 function generateID(type) {
 
@@ -37,18 +183,39 @@ function generateID(type) {
     if(type == 'user') { // 유저 ID 생성
       data[0] = Math.floor(Math.random() * 16) + 16;
     }
-  
+    else if(type == 'space') { // space ID 생성
+      data[0] = Math.floor(Math.random() * 32) + 32;
+    }
+    else if(type == 'router') { // router ID 생성
+      data[0] = Math.floor(Math.random() * 64) + 64;
+    }
+    else if(type == 'device') { // device ID 생성
+      data[0] = Math.floor(Math.random() * 96) + 64;
+    }
+    else if(type == 'beacon') { // beacon ID 생성
+      data[0] = Math.floor(Math.random() * 144) + 80;
+    }
+
     // 나머지 바이트는 0에서 255사이의 랜덤한 값
     for (let i = 1; i < 6; i++) {
       data[i] = Math.floor(Math.random() * 256);
     }
   
-    return "X'" + data.toString('hex') + "'";
+    var genearted_id = "X'" + data.toString('hex') + "'";
+    console.log(`generated id ${type} : ${genearted_id}`);
+    const byteArray = Buffer.from(data.toString('hex'), 'hex');
+    const paddedByteArray = Buffer.alloc(6);
+    byteArray.copy(paddedByteArray, 6 - byteArray.length);
+    for(i = 0; i < paddedByteArray.length; i++) {
+        console.log(byteArray[i]);
+    }
+    console.log(paddedByteArray);
+    return genearted_id;
 
 }
 
 
 module.exports = {
-    insertUser
+    select_db, insert_db, update_db
 };
 
--- SocketIO.js
+++ SocketIO.js
@@ -1,8 +1,11 @@
-var ServerPort = 8710;
+const {insert_db, select_db, update_db} = require('./DBConnector');
+
+
 
 // client 관리 딕셔너리
-    // TEMP : 
 var clients = {};
+var ServerPort = 8710;
+
 
 function startServer() {
 
@@ -12,9 +15,6 @@ function startServer() {
     const io = require('socket.io')(http);
     const { Server } = require('http');
     
-    
-
-
     app.get('/', (req, res) => {
         res.sendFile(__dirname + '/index.html');
     });
@@ -29,18 +29,27 @@ function startServer() {
 
 }
 
+// Session
 function OnConnection(socket) {
+    console.log('-------------------------------');
     console.log(`a user connected : ${socket.id}`);
+    console.log('-------------------------------');
    
     // 고유한 ID 생성
     const clientId = Math.random().toString(36).substring(2, 9);
     // 클라이언트 ID와 소켓 객체 저장
     clients[clientId] = socket;
 
-        
+    
+    // Session 이벤트 등록
     socket.on('test_send', OnTestSend);
     socket.on('test_request', OnTestRequest);
     socket.on('disconnect', OnDisconnect);
+
+    // DB 이벤트 등록
+    socket.on('select', OnSelectDB);
+    socket.on('insert', OnInsertDB);
+    socket.on('update', OnUpdateDB);
 }
 
 function OnTestRequest(data) {
@@ -61,12 +70,75 @@ function OnTestSend(socket) {
     socket.emit('test_send', data);
 }
 
+
 function OnDisconnect(socket, clientId) {
     // 연결이 끊긴 클라이언트 삭제
     delete clients[clientId];
+    console.log('-------------------------------');
     console.log(`user(${clientId}) disconnected`);
+    console.log('-------------------------------');
 }
 
+// DB
+async function OnSelectDB(data) {
+    
+    console.log('-------------------------------');
+    console.log(`select ${data.table}, ${Buffer.from(data.condition).toString('hex')} request`);
+    
+    var results = await select_db(data.table, data.condition);
+    var dbdata = null;
+    if(results == null) { // 조건에 맞는 데이터가 없는 경우 id를 0x00000000 으로 설정
+        console.debug('there are no data');
+        results = {
+            'id': 0x000000
+        }
+    }
+    else {
+        dbdata = {
+            'table': data.table,
+            'result': results[0]
+        }
+    }
+
+    console.log('-------------------------------');
+    this.emit('select_result', dbdata);
+}
+
+async function OnInsertDB(data) {
+    
+    console.log('-------------------------------');
+    console.log(`insert ${data.table} request`);
+    
+    var generated_id = await insert_db(data);
+
+    console.log('-------------------------------');
+    
+    var res_data = {
+        'ID': generated_id,
+        'table': data.table
+    }
+    
+    // 발급된 ID와 함께 재전송
+    this.emit('insert_result', res_data);
+}
+
+async function OnUpdateDB(data) {
+    // 데이터에는 수정할 항목의 ID와 table, 수정하고자하는 col의 이름과 업데이트 값을 담아 가져와야 한다.
+    console.log('-------------------------------');
+    console.log(`update ${data.table}, ${data.column}, ${Buffer.from(data.update_item_id).toString('hex')} request`);
+    
+    var update_item_id = await update_db(data);
+
+    // 업데이트에 대해서는 수정된 아이템의 ID만 같이 전송함
+    console.log('-------------------------------');
+
+    var res_data = {
+        'ID': update_item_id,
+        'table': data.table
+    }
+
+    this.emit('update_result', res_data);
+}
 
 
 module.exports = {startServer};
